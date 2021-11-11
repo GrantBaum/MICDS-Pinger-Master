0fd55e2a87b957f0571fba89ecfa077110a607ed
Grant Baumstark <gbaumstark@micds.org>
2021-11-11 14:50:30 -0600

    Create pinger.py

---
+++ pinger.py
@@ -0,0 +1,22 @@
+from os import system
+import time
+print ('MICDS Pinger Tool')
+time.sleep(3)
+print('Ping MICDS Related Websites to check their status :)')
+time.sleep(3)
+print('1. Ping MyMICDS')
+print('2. Ping MICDS')
+print('3. Ping Canvas')
+print('4. Ping custom URL')
+key = int(input('Input your choice: '))
+if key == 1:
+        system("ping mymicds.net")
+elif key == 2:
+        system("ping www.micds.org")
+elif key == 3:
+    system("ping micds.instructure.com")
+elif key == 4:
+        url = input('Enter URL: ')
+        system("ping " + url)
+else:
+        print("Invalid Option!")
